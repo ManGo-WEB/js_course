2e81ddc9fc13cc80c2c48a186e5ddd6ad49c110e
Nikita Vampilov <info@mango-web.ru>
2024-04-29 17:05:23 +1000

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let money = prompt('Ваш бюджет на месяц?'),
+	time = prompt('Введите дату в формате YYYY-MM-DD');
+
+console.log('money = ' + money);
+console.log('date = ' + time);
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false
+};
+
+let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
+	q2 = prompt('Во сколько обойдется?'),
+	q3 = prompt('Введите обязательную статью расходов в этом месяце'),
+	q4 = prompt('Во сколько обойдется?');
+
+appData.expenses.q1 = q2;
+appData.expenses.q3 = q4;
+
+console.log(appData.budget);
+
+alert(appData.budget / 30);
